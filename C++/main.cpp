5ac648a46b1d44b9db1418ca93f7a9d86ac10a86
Roya Salei <41925879+roysea@users.noreply.github.com>
2022-10-29 12:57:24 -0700

    added c++ code for maze

---
+++ C++/main.cpp
@@ -0,0 +1,52 @@
+/*
+To Complete This Exercise:
+        Write a std::vector<vector<State>> Search function stub which takes a board grid
+        and two length 2 int arrays as arguments.
+        The int arrays will represent the start and goal coordinates for the search.
+        The function should print ""No path found!" and
+        return an empty std::vector<vector<State>>.
+        The function will later return the board with a path from the start to the goal.
+        In main(), call Search with a start of {0, 0} and a goal of {4, 5}.
+        Store the results in the variable solution.
+        Pass solution to PrintBoard.
+*/
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "maze.cpp"
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+using std::abs;
+
+
+// TODO: Write the Heuristic function here.
+int Heuristic(int x1, int y1, int x2, int y2){
+  return abs(x2 - x1) + abs(y2 - y1);
+}
+
+//Implementation of A* search algorithm
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2]) {
+
+
+  cout << "No path found!" << "\n";
+  return std::vector<vector<State>> {};
+}
+
+
+
+
+
+int main() {
+  int init[2]{0, 0};
+  int goal[2]{4, 5};
+  auto board = ReadBoardFile("1.board");
+  auto solution = Search(board, init, goal);
+  PrintBoard(board);
+  PrintBoard(solution);
+}
---
+++ C++/maze.cpp
@@ -0,0 +1,68 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+enum class State {kEmpty, kObstacle, kClosed};
+
+// read every string line, parse digits, and
+// push back states
+vector<State> ParseLine(string line) {
+    istringstream sline(line);
+    int n;
+    char c;
+    vector<State> row;
+    while (sline >> n >> c && c == ',') {
+      // push_back a State:
+        // kEmpty if n is 0, and
+        // kObstacle otherwise.
+      if(n == 0) row.push_back(State::kEmpty);
+      else row.push_back(State::kObstacle);
+
+    }
+    return row;
+}
+
+
+// read board file given the path
+// create a 2D board which will contain State objects (obstacle or empty)
+vector<vector<State>> ReadBoardFile(string path) {
+  ifstream myfile (path);
+  // TODO: Modify the board declarationto store
+  // State objects instead of ints.
+  vector<vector<State>> board{};
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      vector<State> row = ParseLine(line);
+      board.push_back(row);
+    }
+  }
+  return board;
+}
+
+// read state and replace with icon for obstacle
+// and 0 for empty
+string CellString(State cell) {
+  switch(cell) {
+    case State::kObstacle: return "⛰️   ";
+    default: return "0   ";
+  }
+}
+
+void PrintBoard(const vector<vector<State>> board) {
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[i].size(); j++) {
+      // TODO: Modify the line below to call CellString
+      // on each element of the board before streaming to cout.
+      cout << CellString(board[i][j]);
+    }
+    cout << "\n";
+  }
+}
